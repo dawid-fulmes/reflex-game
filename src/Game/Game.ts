d8a4055de55a20d8de73c344ef4ec2b3ac094286
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-10-25 20:51:16 +0200

    Add finish method and game state

--- src/Game/Game.ts
+++ src/Game/Game.ts
@@ -7,6 +7,7 @@ class Game {
   private pointsCounter: Counter;
   private timeCounter: Counter;
   private timeout: number;
+  private state: "INITIAL" | "GAME" | "END";
 
   constructor(
     boardRows: number,
@@ -19,6 +20,7 @@ class Game {
     this.pointsCounter = new Counter("points");
     this.timeCounter = new Counter("time", maxTime);
     this.timeout = null;
+    this.state = "INITIAL";
   }
 
   init(): void {
@@ -26,28 +28,37 @@ class Game {
   }
 
   start(): void {
-    if (this.timeout !== null) {
+    if (this.state !== "INITIAL") {
       return;
     }
 
+    this.state = "GAME";
     const tickTime = (): void => {
       this.timeCounter.decrement();
       if (this.timeCounter.getValue() > 0) {
         this.timeout = window.setTimeout(tickTime, 1000);
       } else {
-        //End Game
+        this.finish();
       }
     };
     this.timeout = window.setTimeout(tickTime, 1000);
   }
 
   reset(): void {
-    if (this.timeout === null) {
+    if (this.state === "INITIAL") {
       return;
     }
 
     window.clearTimeout(this.timeout);
     this.timeout = null;
+    this.state = "INITIAL";
+  }
+
+  finish(): void {
+    this.state = "END";
+    if (this.timeout !== null) {
+      window.clearTimeout(this.timeout);
+    }
   }
 }
 
