6b4eece06f68dba431279177dcd65e4528ee32db
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-10-26 14:21:51 +0200

    Add colorful counters

--- src/Game/Counter.ts
+++ src/Game/Counter.ts
@@ -2,12 +2,35 @@ class Counter {
   private element: HTMLElement;
   private value: number;
   private initialValue: number;
+  private isColorful: boolean;
 
   constructor(type: "lives" | "points" | "time", initialValue: number = 0) {
     this.element = document.getElementById(type);
     this.initialValue = initialValue;
     this.value = initialValue;
     this.element.textContent = this.value.toString();
+    if (type === "points") {
+      this.isColorful = false;
+    } else {
+      this.isColorful = true;
+    }
+    this.updateColor();
+  }
+
+  private updateColor(): void {
+    if (!this.isColorful) {
+      return;
+    }
+    let color: string;
+    if (this.value > (this.initialValue * 2) / 3) {
+      color = "green";
+    } else if (this.value > this.initialValue / 3) {
+      color = "orange";
+    } else {
+      color = "red";
+    }
+
+    this.element.style.color = color;
   }
 
   getValue(): number {
@@ -17,16 +40,19 @@ class Counter {
   decrement(): void {
     this.value = this.value - 1;
     this.element.textContent = this.value.toString();
+    this.updateColor();
   }
 
   increment(): void {
     this.value = this.value + 1;
     this.element.textContent = this.value.toString();
+    this.updateColor();
   }
 
   reset(): void {
     this.value = this.initialValue;
     this.element.textContent = this.value.toString();
+    this.updateColor();
   }
 }
 
